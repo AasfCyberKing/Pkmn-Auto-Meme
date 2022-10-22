f36caaefdb134b0710bc173b693255b9965ba0e3
Aasf [ありがとう] <89995228+AasfCyberKing@users.noreply.github.com>
2022-10-22 11:58:35 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+import os
+import logging
+import asyncio
+
+from AutoPostTelegram import auto
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+
+TOKEN = os.environ.get("TOKEN", None) # Your Bot's Token Get It From @Botfather
+CHAT = os.environ.get("CHAT", None) # Your Group Or Channel's Username With @ Symbol Where It Post Memes
+GAP = os.environ.get("GAP", None) # Time Gap Of Your Post In Second 1 min = 60 sec
+
+# For Local Deploy:
+"""
+TOKEN = ""
+CHAT = ""
+GAP = ""
+"""
+
+app = auto(TOKEN)
+
+while True:
+    app.pkmnmeme(chat=CHAT)
+    asyncio.sleep(GAP) #The Time Gap Of Your Post In Seconds
+    app.pkmnmeme(chat=CHAT)
